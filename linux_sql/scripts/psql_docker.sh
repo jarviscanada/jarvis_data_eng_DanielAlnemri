ec25fd911dba33d4e34ebfecbf0ff30210a8b7ab
Link-248 <d.alnemri@mail.utoronto.ca>
2024-03-19 16:53:33 +0000

    Fix styling issue caused by Intellij

--- linux_sql/scripts/psql_docker.sh
+++ linux_sql/scripts/psql_docker.sh
@@ -19,9 +19,10 @@ case $cmd in
 
   # Check if the container is already created
   if [ $container_status -eq 0 ]; then
-		echo 'Container already exists'
-		exit 1
-	fi
+	echo 'Container already exists'
+	exit 1
+  fi
+
 
   # Check # of CLI arguments
   if [ $# -ne 3 ]; then
@@ -30,12 +31,12 @@ case $cmd in
   fi
 
   # Create container
-	docker volume create pgdata
+  docker volume create pgdata
   # Start the container
-	docker run --name jrvs-psql -e POSTGRES_PASSWORD=$db_password -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres:9.6-alpine
+  docker run --name jrvs-psql -e POSTGRES_PASSWORD=$db_password -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres:9.6-alpine
   # Make sure you understand what's `$?`
-	exit $?
-	;;
+  exit $?
+  ;;
 
   start|stop)
   # Check instance status; exit 1 if container has not been created
@@ -45,13 +46,13 @@ case $cmd in
   fi
 
   # Start or stop the container
-	docker container $cmd jrvs-psql
-	exit $?
-	;;
+  docker container $cmd jrvs-psql
+  exit $?
+  ;;
 
   *)
-	echo 'Illegal command'
-	echo 'Commands: start|stop|create'
-	exit 1
-	;;
+  echo 'Illegal command'
+  echo 'Commands: start|stop|create'
+  exit 1
+  ;;
 esac
